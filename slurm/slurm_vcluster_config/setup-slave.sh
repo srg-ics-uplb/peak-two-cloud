61c990be8dce9b15684022216545b61504e37e0e
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2016-10-05 11:02:48 +0800

    edits

--- slurm/slurm_vcluster_config/setup-slave.sh
+++ slurm/slurm_vcluster_config/setup-slave.sh
@@ -12,7 +12,7 @@ cd /etc/munge
 sudo cp ~/munge.key .
 sudo chown munge: munge.key
 sudo chmod 400 munge.key
-sudo adduser user01
+sudo useradd -m -s /bin/bash user01
 sudo usermod -G mpiuser user01
 sudo service munge restart
 sudo service slurm-llnl restart
