52e44dd7d24ff8152da0ccc9a9ea1a29bc222f3f
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2016-10-06 14:26:19 +0800

    edits

--- slurm/slurm_vcluster_config/setup-slave.sh
+++ slurm/slurm_vcluster_config/setup-slave.sh
@@ -16,5 +16,5 @@ sudo useradd -m -p pazgpwQZNFF9. -s /bin/bash user01
 sudo usermod -G mpiuser user01
 sudo service munge restart
 sudo service slurm-llnl restart
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/*
+su - mpiuser -c 'chmod 700 ~/.ssh'
+su - mpiuser -c 'chmod 600 ~/.ssh/*'
