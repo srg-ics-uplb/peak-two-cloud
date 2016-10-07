4cf6ab26b03ff16be9b5f2f75584cef4276cec62
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2016-10-07 10:56:19 +0800

    add many users

---
+++ slurm/slurm_vcluster_config/addmany.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <users file>"
+    exit
+fi
+
+
+
+IFS=$'\r\n' GLOBIGNORE='*' command eval  'XYZ=($(cat $1))'
+
+for i in "${XYZ[@]}"
+do
+        #echo $i
+        COLS=( $i );
+        ./adduser.sh ${COLS[0]} ${COLS[1]}
+done
+
+
