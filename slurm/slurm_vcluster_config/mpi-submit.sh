c41de8080fa8934a06c1e7df84b909fc24344fc7
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2016-10-07 10:58:11 +0800

    edits

--- slurm/slurm_vcluster_config/mpi-submit.sh
+++ slurm/slurm_vcluster_config/mpi-submit.sh
@@ -5,5 +5,10 @@
 #SBATCH --nodes=4
 #SBATCH --ntasks=4
 
-p2c-mpi-slurm hello.c 2
+
+mpicc -o hello.exe hello.c
+mpiexec -np 4 -f ../nodes.txt ./hello.exe
+
+
+#p2c-mpi-slurm hello.c 2
 #p2c-mpi-slurm mpi_array.c 4
