987d1ebe9d9d6c2596e5500851515508899aa1bd
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2016-10-07 11:54:56 +0800

    edits

--- slurm/slurm_vcluster_config/hello-submit.sh
+++ slurm/slurm_vcluster_config/hello-submit.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 #
-#SBATCH --job-name=mpijob
-#SBATCH --output=result.txt
+#SBATCH --job-name=hello
+#SBATCH --output=hello.out
 #SBATCH --nodes=4
 #SBATCH --ntasks=4
-
 
 mpicc -o hello.exe hello.c
 mpiexec -np 4 -f ../nodes.txt ./hello.exe
 
 
-#p2c-mpi-slurm hello.c 2
-#p2c-mpi-slurm mpi_array.c 4
