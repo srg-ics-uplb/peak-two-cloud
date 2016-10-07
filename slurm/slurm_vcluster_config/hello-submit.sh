c2ae4996be3a7f76fee48d57cefc676efff18629
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2016-10-07 12:11:40 +0800

    edits

--- slurm/slurm_vcluster_config/hello-submit.sh
+++ slurm/slurm_vcluster_config/hello-submit.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 #
+
+#the name of the job
 #SBATCH --job-name=hello
+
+#the stdout output of the job
 #SBATCH --output=hello.out
+
+#available nodes
 #SBATCH --nodes=4
+
+#number of tasks
 #SBATCH --ntasks=4
 
-mpicc -o hello.exe hello.c
-mpiexec -np 4 -f ../nodes.txt ./hello.exe
+#the source file
+SOURCE=hello.c
 
+#the executable
+EXEC=hello.exe
 
+#number of nodes to use in the run
+NODES=4
+
+#compile and execute
+mpicc -o $EXEC $SOURCE
+mpiexec -np $NODES -f ../nodes.txt ./$EXEC
