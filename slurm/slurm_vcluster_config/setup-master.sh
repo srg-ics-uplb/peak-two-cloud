e23776fee7c045cb2d65937c83b03c246fea7e4e
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2016-10-12 13:09:19 +0800

    edits

--- slurm/slurm_vcluster_config/setup-master.sh
+++ slurm/slurm_vcluster_config/setup-master.sh
@@ -18,7 +18,11 @@ cd /etc/slurm-llnl
 cat /etc/hosts #take note  of the host names of the master and the slaves
 sudo wget https://github.com/srg-ics-uplb/peak-two-cloud/raw/master/slurm/slurm_vcluster_config/slurm.conf
 #Edit the ControlMachine and COMPUTE NODES entries
-sed -i "s/cmsc180/$1/g" slurm.conf
+CLUSTER_MASTER=`hostname`
+SUFFIX="-mpi-master"
+CLUSTER_NAME=${CLUSTER_MASTER%$SUFFIX}
+echo "Setting cluster name to $CLUSTER_NAMEi.."
+sed -i "s/cmsc180/$CLUSTER_NAME/g" slurm.conf
 sudo chmod 775 /mirror
 cd /usr/bin
 sudo wget https://github.com/srg-ics-uplb/peak-two-cloud/raw/master/slurm/slurm_vcluster_config/p2c-mpi-slurm
