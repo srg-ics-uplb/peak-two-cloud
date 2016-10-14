873cbd4d2b00bfd4d48ac51f2393f9b43a8a519c
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2016-10-14 19:35:19 +0800

    added generic submit script

---
+++ slurm/slurm_vcluster_config/submit.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+
+
+f [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <executable>"
+    exit
+fi
+
+
+#the name of the job
+#SBATCH --job-name=hello
+
+#the stdout output of the job
+#SBATCH --output=hello.out
+
+#available nodes
+#SBATCH --nodes=4
+
+#number of tasks
+#SBATCH --ntasks=4
+
+#time limit (D-HH:MM), terminate the job after two minutes
+#if not yet done
+#SBATCH -t 0-0:02
+
+#the executable
+EXEC=$1
+
+#number of nodes to use in the run
+NODES=4
+
+#execute
+mpiexec -np $NODES -f ../nodes.txt ./$EXEC
