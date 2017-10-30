0b5df8ee9e97a6075e5c17bcc78f535376a2a557
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2017-10-30 11:46:49 +0800

    edits

--- slurm/slurm_vcluster_config/adduser.sh
+++ slurm/slurm_vcluster_config/adduser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <username> <password>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <username> <password> <id>"
     exit
 fi
 
@@ -11,5 +11,6 @@ PASSHASH=`perl -e 'printf("%s\n", crypt($ARGV[0], "password"))' "$2"`
 MPIUSER_PASS=mpiuser_password
 
 echo $MPIUSER_PASS | sudo -S useradd -m -d /mirror/$1 -p $PASSHASH -s /bin/bash $1
-echo $MPIUSER_PASS | sudo -S usermod -G mpiuser $1
+echo $MPIUSER_PASS | sudo -S groupmod -g $3 mpiuser $1
+echo $MPIUSER_PASS | sudo -S usermod -u $3 -g $3 -G mpiuser $1
 echo $MPIUSER_PASS | sudo -S chmod 711 /mirror/$1
