db34330251c01c9dff83fcdb3e434fd2232d5b1c
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2018-10-24 13:14:17 +0800

    added nodename in hello.c

--- slurm/slurm_vcluster_config/hello.c
+++ slurm/slurm_vcluster_config/hello.c
@@ -2,14 +2,18 @@
 #include <mpi.h>
 
 int main(int argc, char** argv) {
-    int myrank, nprocs;
-
+    int myrank, nprocs,len;
+    char nodename[128];
+ 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 
-    printf("Hello from processor %d of %d\n", myrank, nprocs);
+    MPI_Get_processor_name(nodename, &len);
+    nodename[len]='\0';
+    printf("Hello from processor %d (%s) of %d\n", myrank, nodename,nprocs);
 
     MPI_Finalize();
     return 0;
 }
+
