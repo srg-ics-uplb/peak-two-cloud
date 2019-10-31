f3b42429f94f89c3ee0f74d5cffe2a576a5703ab
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2019-10-31 16:00:58 +0800

    edits

--- nova-api-test/start-instance.py
+++ nova-api-test/start-instance.py
@@ -6,9 +6,9 @@ from credentials import get_nova_creds
 
 creds = get_nova_creds()
 nova = novaclient.Client("2",**creds)
-image = nova.images.find(name="Ubuntu-14.04-server-amd64")
-flavor = nova.flavors.find(name="p2c.1_512_5_1_1")
-instance = nova.servers.create(name="frompython", image=image, flavor=flavor, key_name="jachermocilla-p2c")
+image = nova.images.find(name="Ubuntu-16.04-server-amd64")
+flavor = nova.flavors.find(name="p2c.1_512_20_1_1")
+instance = nova.servers.create(name="frompython", image=image, flavor=flavor, key_name="guest-key")
 
 # Poll until the status is no longer 'BUILD'
 status = instance.status
